c004755fd1463eb5a03a11cbeb4cfea76d88600b
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-10 10:52:22 +0700

    chore: update dependencies and setup CI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxthub/core",
+    "@nuxt/hints",
+    "@nuxt/a11y",
+    "@nuxt/icon",
+    "@nuxt/image",
+    "@nuxt/fonts",
+    "@nuxt/eslint",
+  ],
+  hub: {
+    db: "postgresql",
+  },
+});
