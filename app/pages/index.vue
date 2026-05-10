da90b9d6a2fc29276210c860dd9d028dd4613c31
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-10 13:53:32 +0700

    chore: setup nuxt ui

---
+++ app/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    Landing page
+    <UButton>Test button</UButton>
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,19 +2,24 @@
 export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
   modules: [
     "@nuxthub/core",
     "@nuxt/hints",
     "@nuxt/a11y",
-    "@nuxt/icon",
     "@nuxt/image",
-    "@nuxt/fonts",
     "@nuxt/eslint",
+    "@nuxt/ui",
   ],
   hub: {
     db: "postgresql",
     cache: true,
     kv: true,
     blob: true,
+  },
+  ui: {
+    experimental: {
+      componentDetection: true,
+    },
   },
 });
