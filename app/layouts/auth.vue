ea82d0af83fc79d0656af95cc49c7c2d385626d6
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-11 23:01:44 +0700

    chore: update auth layout

--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,5 +1,10 @@
 <template>
-  <main class="flex min-h-screen items-center justify-center px-4 py-12">
-    <slot />
-  </main>
+  <UContainer>
+    <header class="flex items-center justify-end mt-4">
+      <UColorModeSelect />
+    </header>
+    <main class="flex min-h-screen items-center justify-center px-4 py-12">
+      <slot />
+    </main>
+  </UContainer>
 </template>
