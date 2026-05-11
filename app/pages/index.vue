bfa6a206756c803584d336190383a86fe529fdfb
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-11 11:13:19 +0700

    chore: update editor config and format code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,14 +3,9 @@
     <h1 class="text-2xl font-semibold">Trang chủ</h1>
     <p v-if="session.data">
       Chào mừng, {{ session.data.user.name }}! Bạn đã đăng nhập thành công.
-      <UButton
-        class="ml-4"
-        color="error"
-        variant="outline"
-        size="sm"
-        @click="authClient.signOut()"
-        >Đăng xuất</UButton
-      >
+      <UButton class="ml-4" color="error" variant="outline" size="sm" @click="authClient.signOut()">
+        Đăng xuất
+      </UButton>
     </p>
     <UButton to="/signin">Sign in</UButton>
   </div>
