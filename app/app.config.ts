a15ed2da5236b3d051267a03dddecb3a50fdde1b
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-11 23:15:02 +0700

    feat: implement authentication UI configuration and enhance sign-in/sign-up forms

---
+++ app/app.config.ts
@@ -0,0 +1,9 @@
+export default defineAppConfig({
+  ui: {
+    authForm: {
+      slots: {
+        header: "flex items-center justify-center",
+      },
+    },
+  },
+});
--- app/layouts/auth.vue
+++ app/layouts/auth.vue
@@ -1,9 +1,10 @@
 <template>
   <UContainer>
-    <header class="flex items-center justify-end mt-4">
+    <header class="flex items-center justify-between mt-4">
+      <NuxtImg src="/images/logo-with-text.png" width="150" height="50" alt="Logo with text" />
       <UColorModeSelect />
     </header>
-    <main class="flex min-h-screen items-center justify-center px-4 py-12">
+    <main class="flex justify-center mt-20">
       <slot />
     </main>
   </UContainer>
