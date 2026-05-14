7a9a2d37ce123423c024da8a59becffc249d53f7
Vo Quang Chien <2giosangmitom@proton.me>
2026-05-14 23:11:54 +0700

    fix: use `@nuxt/fonts` to for font management

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,25 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
-  app: {
-    head: {
-      link: [
-        {
-          rel: "preconnect",
-          href: "https://fonts.googleapis.com",
-        },
-        {
-          rel: "preconnect",
-          href: "https://fonts.gstatic.com",
-          crossorigin: "",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Plus+Jakarta+Sans:wght@600;700&display=swap",
-        },
-      ],
-    },
-  },
   modules: [
     "@nuxthub/core",
     "@nuxt/hints",
